942f999dd6686872e246074ca80f841e1234acc2
prashantk0993 <34300732+prashantk0993@users.noreply.github.com>
2024-10-08 15:49:49 +0100

    test commit

---
+++ src/main/java/com/pra/Interview/ByteAddition.java
@@ -0,0 +1,12 @@
+package com.pra.Interview;
+
+public class ByteAddition {
+
+    public static void main(String[] args) {
+        byte a=1;
+        byte b=1;
+        byte c= a+b;
+
+        System.out.println(c);
+    }
+}
